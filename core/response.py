68960427dae5aebf99232e170fba253a0b6e84dc
lihuacai <lihuacai168@gmail.com>
2023-08-19 11:37:46 +0800

    feat(core): remove err_code field, change dict type to None field when error
    BREAKING CHANGE: change stand response field

--- core/response.py
+++ core/response.py
@@ -1,3 +1,4 @@
+"""分页核心函数"""
 from django.core.paginator import Paginator
 from django.db.models import QuerySet
 
@@ -6,11 +7,12 @@ from core.schemas import PageFilter
 
 
 def get_page(
-        qs: QuerySet,
-        f: PageFilter,
-        t: GenericResultsType) -> PageSchema:
-    p = Paginator(qs, per_page=f.page_size).get_page(f.page_index)
-    return PageSchema[t](
+        queryset: QuerySet,
+        pager_filter: PageFilter,
+        generic_result_type: GenericResultsType) -> PageSchema:
+    """标准分页"""
+    p = Paginator(queryset, per_page=pager_filter.page_size).get_page(pager_filter.page_index)
+    return PageSchema[generic_result_type](
         total=p.paginator.count,
         page_size=p.paginator.per_page,
         page_index=p.number,
