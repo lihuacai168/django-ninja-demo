ebb35b1be0038a382bb9f5de0cd5b41ba9c89e7d
rikasai233 <rikasai233@gmail.com>
2024-04-21 15:33:22 +0800

    feat: impl curd router

---
+++ core/router.py
@@ -0,0 +1,77 @@
+from typing import Any, List, Optional, Type, TypeVar, Union
+
+from core.schemas import DictId, PageFilter, PageSchema, StandResponse
+from core.service import GenericCURD
+from ninja import Body, Query, Router, Schema
+from ninja.constants import NOT_SET_TYPE
+
+TPageFilter = TypeVar("TPageFilter", bound=PageFilter)
+TSchema = TypeVar("TSchema", bound=Schema)
+
+
+class CRUDRouter(Router):
+    def __init__(
+        self,
+        service_impl: GenericCURD,
+        filters_class: Type[TPageFilter],
+        in_schema: Type[TSchema],
+        out_schema: Type[TSchema],
+        path: str = "",
+        tags: Optional[List[str]] = None,
+        auth: Any = NOT_SET_TYPE,
+    ):
+        super().__init__(tags=tags, auth=auth)
+        self.service_impl = service_impl
+        self.filters_class = filters_class
+        self.in_schema = in_schema
+        self.out_schema = out_schema
+        self.path = path
+        self.register_crud_routes()
+
+    def register_crud_routes(self):
+        # create an obj
+        @self.post(self.path, response=StandResponse[Union[DictId, None]])
+        def create_obj(request, payload: self.in_schema):
+            return self.service_impl.create_obj(
+                payload, request.user.email or request.user.username
+            )
+
+        # get an obj
+        @self.get(
+            f"{self.path}/{{id}}", response=StandResponse[Union[self.out_schema, None]]
+        )
+        def get_obj(request, id: int):
+            return self.service_impl.get_obj(id)
+
+        # get a list of objs
+        @self.get(self.path, response=StandResponse[PageSchema[self.out_schema]])
+        def list_obj(request, filters: self.filters_class = Query(...)):
+            objs = self.service_impl.list_obj(filters, self.out_schema)
+            return StandResponse(data=objs)
+
+        # full update obj
+        @self.put(
+            f"{self.path}/{{id}}",
+            response=StandResponse[Union[DictId, None]],
+            description="full obj update",
+        )
+        def update_obj(request, id: int, payload: self.in_schema):
+            return self.service_impl.update_obj(
+                id, payload, request.user.email or request.user.username
+            )
+
+        # partial update obj
+        @self.patch(
+            f"{self.path}/{{id}}",
+            response=StandResponse[Union[DictId, None]],
+            description="partial obj update",
+        )
+        def partial_update_obj(request, id: int, payload: dict = Body(...)):
+            return self.service_impl.partial_update(
+                id, request.user.email or request.user.username, **payload
+            )
+
+        # delete an obj
+        @self.delete(f"{self.path}/{{id}}", response=StandResponse[bool])
+        def delete_obj(request, id: int):
+            return self.service_impl.delete_obj(id)
--- employee/views.py
+++ employee/views.py
@@ -1,17 +1,16 @@
 """employee curd view"""
 import logging
-from typing import Optional, Union
+from typing import Optional
 
-from ninja import Query, Router, Schema
+from ninja import Schema
 from ninja_jwt.authentication import JWTAuth
 from pydantic.fields import Field
 from pydantic.types import conint
 
-from core.schemas import DictId, PageSchema, StandResponse
+from core.router import CRUDRouter
 from employee.employee_service_impl import employee_service_impl
 from employee.schemas import EmployeeFilters, EmployeeIn, EmployeeOut
 
-router = Router(tags=["employees"], auth=JWTAuth())
 
 logger = logging.getLogger(__name__)
 
@@ -22,33 +21,44 @@ class Filters(Schema):
     department_id: Optional[conint(ge=0)]
 
 
-@router.post("/employees", response=StandResponse[Union[DictId, None]])
-def create_employee(request, payload: EmployeeIn):
-    logger.info(f"input: payload={payload.dict()}")
-    return employee_service_impl.create_obj(payload, "huacai")
-
-
-@router.get(
-    "/employees/{employee_id}", response=StandResponse[Union[EmployeeOut, None]]
+# CRUDRouter, like DRF model views set
+router = CRUDRouter(
+    service_impl=employee_service_impl,
+    filters_class=EmployeeFilters,
+    in_schema=EmployeeIn,
+    out_schema=EmployeeOut,
+    path="/employees",
+    tags=["employees"],
+    auth=JWTAuth()
 )
-def get_employee(request, employee_id: int):
-    return employee_service_impl.get_obj(employee_id)
 
-
-@router.get("/employees", response=StandResponse[PageSchema[EmployeeOut]])
-def list_employees(request, filters: EmployeeFilters = Query(...)):
-    logger.info(f"input: filters={filters.dict()}")
-    objs = employee_service_impl.list_obj(filters, EmployeeOut)
-    return StandResponse(data=objs)
-
-
-@router.put("/employees/{employee_id}", response=StandResponse[Union[DictId, None]])
-def update_employee(request, employee_id: int, payload: EmployeeIn):
-    payload.department_id = employee_id
-    logger.info(f"input: payload={payload.dict()}")
-    return employee_service_impl.update_obj(employee_id, payload, "huacai")
-
-
-@router.delete("/employees/{employee_id}", response=StandResponse[bool])
-def delete_employee(request, employee_id: int):
-    return employee_service_impl.delete_obj(employee_id)
+# @router.post("/employees", response=StandResponse[Union[DictId, None]])
+# def create_employee(request, payload: EmployeeIn):
+#     logger.info(f"input: payload={payload.dict()}")
+#     return employee_service_impl.create_obj(payload, "huacai")
+#
+#
+# @router.get(
+#     "/employees/{employee_id}", response=StandResponse[Union[EmployeeOut, None]]
+# )
+# def get_employee(request, employee_id: int):
+#     return employee_service_impl.get_obj(employee_id)
+#
+#
+# @router.get("/employees", response=StandResponse[PageSchema[EmployeeOut]])
+# def list_employees(request, filters: EmployeeFilters = Query(...)):
+#     logger.info(f"input: filters={filters.dict()}")
+#     objs = employee_service_impl.list_obj(filters, EmployeeOut)
+#     return StandResponse(data=objs)
+#
+#
+# @router.put("/employees/{employee_id}", response=StandResponse[Union[DictId, None]])
+# def update_employee(request, employee_id: int, payload: EmployeeIn):
+#     payload.department_id = employee_id
+#     logger.info(f"input: payload={payload.dict()}")
+#     return employee_service_impl.update_obj(employee_id, payload, "huacai")
+#
+#
+# @router.delete("/employees/{employee_id}", response=StandResponse[bool])
+# def delete_employee(request, employee_id: int):
+#     return employee_service_impl.delete_obj(employee_id)
